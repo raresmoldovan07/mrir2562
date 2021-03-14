5f5f6f52e93c79b80e88c7225488169152d27aae
Vlad Pascu <vlad_pascu25@yahoo.com>
2021-03-14 18:52:03 +0200

    refactor reading from file

--- src/main/java/inventory/controller/AddPartController.java
+++ src/main/java/inventory/controller/AddPartController.java
@@ -24,8 +24,7 @@ public class AddPartController implements Initializable, Controller {
     private Stage stage;
     private Parent scene;
     private boolean isOutsourced = true;
-    private String errorMessage = new String();
-    private int partId;
+    private String errorMessage = "";
 
     private InventoryService service;
     
@@ -59,8 +58,6 @@ public class AddPartController implements Initializable, Controller {
     @FXML
     private TextField minTxt;
 
-    public AddPartController(){}
-
     @Override
     public void setService(InventoryService service){
 
@@ -84,7 +81,6 @@ public class AddPartController implements Initializable, Controller {
     private void displayScene(ActionEvent event, String source) throws IOException {
         stage = (Stage)((Button)event.getSource()).getScene().getWindow();
         FXMLLoader loader= new FXMLLoader(getClass().getResource(source));
-        //scene = FXMLLoader.load(getClass().getResource(source));
         scene = loader.load();
         Controller ctrl=loader.getController();
         ctrl.setService(service);
@@ -106,7 +102,7 @@ public class AddPartController implements Initializable, Controller {
         alert.setHeaderText("Confirm Cancelation");
         alert.setContentText("Are you sure you want to cancel adding part?");
         Optional<ButtonType> result = alert.showAndWait();
-        if(result.get() == ButtonType.OK) {
+        if(result.isPresent() && result.get() == ButtonType.OK) {
             System.out.println("Ok selected. Part addition canceled.");
             displayScene(event, "/fxml/MainScreen.fxml");
         } else {
